79c66b7b4a871f9dc4e61f75c3516faba00d545a
cklim <chinkwang.lim@gmail.com>
2011-08-15 02:05:04 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreditApp::Application.config.secret_token = '42fb1508023297b0f368cf774777971f437826e9b97f5e9cdccac289c3702cdcca8f681a94deb589700dd088eec3553183e130f3ed088c5342c2d087081e5e97'
